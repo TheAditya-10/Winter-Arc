aae793e83fa4d5d5676bec7712813b2d54e30ff3
TheAditya-10 <aditya.pratap.singh.tomar.1082006@gmail.com>
2026-01-01 02:33:41 +0530

    Cache revalidation

--- client/src/lib/dal/cache.js
+++ client/src/lib/dal/cache.js
@@ -5,24 +5,27 @@ import { getChallengesInfoById, getAllTasks, getTaskInfoById } from "./challenge
 
 export const getChallengesInfoCacheById = unstable_cache(
     async (challengeId) => await getChallengesInfoById(challengeId),
-    [],
+    ['get-challenges-info'],
     {
-        tags: ["get-challenges-info-by-id"]
+        tags: ["get-challenges-info-by-id"],
+        revalidate: 3600 // Revalidate every hour
     }
 )
 
 export const getAllTasksCache = unstable_cache(
     async (challengeId) => await getAllTasks(challengeId),
-    [],
+    ['get-all-tasks'],
     {
-        tags: ["get-all-tasks"]
+        tags: ["get-all-tasks"],
+        revalidate: 3600 // Revalidate every hour
     }
 )
 
 export const getTaskInfoCacheById = unstable_cache(
     async (taskId) => await getTaskInfoById(taskId),
-    [],
+    ['get-task-info'],
     {
-        tags: ["get-task-info-by-id"]
+        tags: ["get-task-info-by-id"],
+        revalidate: 3600 // Revalidate every hour
     }
 )
