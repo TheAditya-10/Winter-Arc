d8af1d7c8a5a168a3db21b065901db9cf0c3d2e4
Deepesh Gupta <learning.is.first.earning@gmail.com>
2026-01-01 13:14:11 +0530

    fix connect with linkedin

--- client/src/app/api/revalidate/route.js
+++ client/src/app/api/revalidate/route.js
@@ -1,12 +1,18 @@
 import { NextResponse } from "next/server"
-import { revalidateTag } from "next/cache"
+import { revalidateTag, revalidatePath } from "next/cache"
 
 
 export async function POST(request) {
     try{
-        const { tag } = await request.json()
-        revalidateTag(tag)
-        return NextResponse.json({ message: "Cache revalidated successfully!!" })
+        const { tag, path, type } = await request.json()
+        if(tag){
+            revalidateTag(tag)
+            return NextResponse.json({ message: `${tag} Cache revalidated successfully!!` })
+        }
+        if(path, type) {
+            revalidatePath(path, type)
+            return NextResponse.json({ message: `${path} ${type} Cache revalidated successfully!!` })
+        }
     } catch (error) {
         console.log(error)
         return NextResponse.json({message: "failed to revalidate cache!!"})
