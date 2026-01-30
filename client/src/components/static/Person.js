6f3244dc2939c0301cd4692ced247c893baa7991
Deepesh Gupta <learning.is.first.earning@gmail.com>
2026-01-30 14:28:00 +0530

    fix: update committee member name and improve layout in Person component

--- client/src/components/static/Person.js
+++ client/src/components/static/Person.js
@@ -3,18 +3,18 @@ import Link from 'next/link'
 
 const Person = ({ img, name, position, github, linkedin, info }) => {
     return (
-        <div className='pt-6 pb-6 px-6 bg-gradient-to-br from-[#1a1d35] to-[#0f1020] border-2 border-[#7689C9]/50 rounded-4xl shadow-xl' >
+        <div className='pt-3 pb-3 px-3 bg-gradient-to-br from-[#1a1d35] to-[#0f1020] border-2 border-[#7689C9]/50 rounded-4xl shadow-xl' >
             <div className='flex justify-between'>
-                <div className='w-30 h-30 overflow-hidden'>
-                    <img className='h-30 w-30 rounded-full border-2 border-[#7689C9]/30' src={img} />
+                <div className='w-28 h-28 overflow-hidden rounded-full border-2 border-[#7689C9]/30'>
+                    <img className='w-full h-full object-cover' src={img} />
                 </div>
 
-                <div className='flex flex-col justify-between'>
+                <div className='flex flex-col justify-between '>
                     <div className='flex gap-2 justify-end'>
                         { !!linkedin && <Link href={linkedin} ><img className='h-6' src="/red_linkedin.svg" /></Link>}
                         { !!github && <Link href={github} ><img className='h-6' src="/red_github.svg" /></Link>}
                     </div>
-                    <div className='text-right'>
+                    <div className='text-right max-w-40'>
                         <p className='text-white font-semibold'>{name}</p>
                         {!!position && <p className='text-[#8ACADE] text-sm'>{position}</p>}
                     </div>
